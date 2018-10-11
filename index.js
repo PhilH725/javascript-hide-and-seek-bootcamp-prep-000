5a05bed96db5b56d1c642d93db715744ccf60e85
Phil Hartley <philh725@gmail.com>
2018-10-11 06:20:50 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
+
+function nestedTarget() {
+  const start = document.getElementById('nested')
+  return start.getElementsByTagName('div')[3]
+}
+
+function increaseRankBy(n) {
+  const allNum = document.getElementById('app').querySelectorAll('ul.ranked-list li')
+  for (let i = 0; i < allNum.length; i++) {
+    allNum[i].innerHTML = parseInt(allNum[i].innerHTML) + n
+  }
+}
+
+function deepestChild() {
+  const start = document.getElementById('grand-node').querySelectorAll('div')
+  for (let i = 0; i < start.length; i++) {
+    if (i === start.length - 1) {
+      return start[i]
+    }
+  }
+}
